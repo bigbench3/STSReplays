3cc62e7a277edee0940165cf6645c233c66ee5d6
Ben Hay <bigbench3@gmail.com>
2026-09-01 19:32:33 -0400

    initial commit

---
+++ src/main/java/com/bhay/stsreplay/StsreplayApplication.java
@@ -0,0 +1,13 @@
+package com.bhay.stsreplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsreplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsreplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhay/stsreplay/StsreplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhay.stsreplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsreplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
